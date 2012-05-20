1ea1dc106d0bd521d1451d97841b955708c0cac4
Eric Wieser <wieser.eric@gmail.com>
2012-05-20 19:24:43 +0100

    Working version currently on the web

---
+++ get.php
@@ -0,0 +1,61 @@
+<?php
+//
+// The PHP curl module supports the received page to be returned in a variable
+// if told.
+//
+
+function presentToUser($data, $asset) {
+	if(preg_match("/^\x89\x50\x4E\x47\x0D\x0A\x1A\x0A/", $data)) {
+		header('Content-Type: image/png');
+		echo $data;
+	}
+	elseif(preg_match("/^version 1.00/", $data)) {
+		header('Content-Type: model/x-roblox-mesh');
+		header('Content-Disposition: attachment; filename="roblox-mesh-'.$asset.'.mesh"');
+		echo $data;
+	}
+	elseif(preg_match("/^ID3/", $data)) {
+		header('Content-Type: audio/mp3');
+		echo $data;
+	}
+	else {
+		header('Content-Type: text/xml');
+		header('Content-Disposition: attachment; filename="roblox-model-'.$asset.'.rbxm"');
+		echo $data;
+	}
+}
+
+if(isset($_GET['asset'])) {
+
+	$asset = @$_GET['asset'];
+
+	$ch = curl_init("http://www.roblox.com/asset/?id=$asset");
+	curl_setopt($ch, CURLOPT_USERAGENT, "Roblox/WinInet");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	$matches = array();
+	preg_match("@http://.{4}\.roblox\.com/[a-f0-9]{32}@i", $result, $matches);
+	
+	
+	if(count($matches) > 0) {
+		if(isset($_GET['showurl'])) {
+			echo $matches[0];
+		}
+		else {
+			$ch = curl_init($matches[0]);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$data = curl_exec($ch);
+			curl_close($ch);
+			
+			presentToUser($data, $asset);
+		}
+		/*
+		echo file_get_contents($matches[0]);*/
+	}
+	else {
+		echo $result;
+	}
+}
+?>
---
+++ index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Roblox Asset Downloader</title>
+		<link href='http://fonts.googleapis.com/css?family=Muli:400,400italic' rel='stylesheet' type='text/css'>
+		<style type="text/css">
+			body {
+				line-height: 20px;
+				background: #F0F0F0;
+				margin: 0;
+				padding: 0;
+				font-family: Muli, sans-serif;
+			}
+			.button {
+				border: 1px solid #c0c0c0;
+				color: #808080;
+				background: #e0e0e0;
+				padding: 4px;
+				display: inline-block;
+				border-radius: 5px;
+				text-decoration: none;
+				margin:5px;
+			}
+			p { margin: 0; padding: 5px; }
+			
+			.button:hover {
+				border: 1px solid #808080;
+				color: #404040;
+			}
+			
+			footer {
+				position: absolute;
+				padding: 5px;
+				background:  #e0e0e0;
+				bottom: 0;
+				left: 0;
+				right: 0;
+			}
+			
+			header {
+				background: #606060 url(http://eric-wieser.tk/images/logo.png) no-repeat 5px 50%;
+				color: white;
+				padding: 5px;
+				overflow: hidden;
+				padding-left: 50px;
+			}
+			
+			header h1 {
+				font-size: 40px;
+				line-height: 40px;
+				padding: 0;
+				margin: 0;
+				float: left;
+			}
+			header p {
+				margin: 0;
+				font-style: italic;
+				line-height: 40px;
+				float: right;
+			}
+			
+			.content {
+				padding: 5px;
+			}
+			
+			.centered {
+				position: absolute;
+				left:50%;
+				top:50%;
+			}
+			.centered .center {
+				position: relative;
+				left: -50%;
+				top: -50%;
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<header>
+			<h1>Roblox Asset downloader</h1>
+			<p>Download Roblox models straight from your browser</p>
+		</header>
+		<section class="content centered">
+			<div class="center">
+				<p>Drag this to your bookmarks or favourites bar:</p>
+				<a draggable="true" class="button" href="javascript:(function(){location.href='http://roblox-asset.comoj.com/'+location.href.match(/-item\?id=(\d+)/i)[1]})()">Download Roblox Asset</a>
+				<p>Then click it when viewing an asset detail page</p>
+			</div>
+		</section>
+		<footer>
+			<a href="http://eric-wieser.tk">More projects...</a>
+		</footer>
+	</body>
+</html>
